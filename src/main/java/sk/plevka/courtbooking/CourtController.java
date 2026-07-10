350577883752d64b9de39257f2b200ee4982e616
David <141168015+Ha-Khu@users.noreply.github.com>
2026-07-10 21:46:26 +0200

    feat: add POST /courts endpoint to create courts

--- src/main/java/sk/plevka/courtbooking/CourtController.java
+++ src/main/java/sk/plevka/courtbooking/CourtController.java
@@ -1,6 +1,8 @@
 package sk.plevka.courtbooking;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -16,5 +18,10 @@ public class CourtController {
     public List<Court> getAllCourts(){
         return courtRepository.findAll();
     }
+
+    @PostMapping("/courts")
+        public Court addCourt(@RequestBody Court court){
+            return courtRepository.save(court);
+        }
 
 }
